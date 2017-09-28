3ad614bd16311cf8ae3a93fd2a1ef3764f63e0a6
Daniel Sawyer <sawyermade@gmail.com>
2017-09-28 10:39:39 -0400

    added comments

--- as4/bigfiles.py
+++ as4/bigfiles.py
@@ -4,10 +4,12 @@ Daniel Sawyer Assignment 4 bigfiles Fall 2017
 import os
 
 def bigfiles(basepth):
-
-	maxfsize = 104857600
+	# file size we are looking for and file list
+	minfsize = 104857600
 	flist = []
 
+	# goes through all dir and files and finds files >= minfilesize
+	# then returns a list of those files
 	for root, dirs, files in os.walk(basepth):
 		for name in files:
 			if root == basepth:
@@ -15,10 +17,11 @@ def bigfiles(basepth):
 			else:
 				fpath = root + '/' + name
 			fsize = os.path.getsize(fpath)
-			if fsize >= maxfsize:
+			if fsize >= minfsize:
 				flist.append(fpath)
 	return flist
 
+# main funtion to test it out, i had a temp dir in the same dir as bigfiles.py
 flist = bigfiles('temp/')
 for name in flist:
 	print(name)
