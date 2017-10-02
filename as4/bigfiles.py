cf2fe072903b8bc9946493a2163dd44278995d5f
Daniel Sawyer <sawyermade@gmail.com>
2017-10-02 19:35:55 -0400

    fixed size shit

--- as4/bigfiles.py
+++ as4/bigfiles.py
@@ -6,14 +6,15 @@ import os
 def bigfiles(basepth):
 	
 	#file size we are looking for and file list
-	minfsize = 104857600
+	# minfsize = 100000000 #1000^2 * 100
+	minfsize = 104857600 #1024^2 * 100
 	flist = []
 
 	#goes through all dir and files and finds files >= minfilesize
 	for root, dirs, files in os.walk(basepth):
 		for name in files:
 			fpath = os.path.join(root, name)
-			if os.path.getsize(fpath) >= minfsize:
+			if os.path.getsize(fpath) > minfsize:
 				flist.append(fpath)
 	
 	#returns list of files >= minfsize
